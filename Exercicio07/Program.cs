7ff5f2f9c30411e53a5d372825e0afcf6be7564e
Alberto <albertomaronezio@hotmai.com>
2019-05-28 16:53:09 -0300

    Exercicio07

---
+++ Exercicio07/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            int sucessor, antecessor = 0;
+            //Recebe os dados
+            Console.Write("Digite um Número: ");
+            numero = int.Parse(Console.ReadLine());
+            //Processa
+            sucessor = numero + 1;
+            antecessor = numero - 1;
+            //Imprime
+            Console.WriteLine("Sucessor do Número é: {0}", sucessor);
+            Console.WriteLine("Antecessor do Número é: {0}", antecessor);
+        }
+    }
+}
